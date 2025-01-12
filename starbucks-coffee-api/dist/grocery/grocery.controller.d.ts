5709ea644d98976c0fa9ccb9d69313be6aeb804d
Nantes0022 <Jubert0022nantes0022@gmail.com>
2025-01-12 14:16:01 +0800

    open-api(updated)

--- starbucks-coffee-api/dist/grocery/grocery.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { GroceryService } from './grocery.service';
-export declare class GroceryController {
-    private readonly groceryService;
-    constructor(groceryService: GroceryService);
-    getGroceryPricing(): Promise<any>;
-    getGroceryByName(name: string): Promise<any>;
-}
--- starbucks-coffee-api/dist/grocery/grocery.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-export declare class GroceryService {
-    private readonly httpService;
-    private readonly apiHost;
-    private readonly apiKey;
-    constructor(httpService: HttpService);
-    getGroceryPricing(): Promise<any>;
-    getGroceryByName(name: string): Promise<any>;
-}
